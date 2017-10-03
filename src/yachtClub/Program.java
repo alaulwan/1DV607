fe4924e04d2480afe456893efe71e66bf3285ca9
Alaa <alaulwan@hotmail.com>
2017-10-03 05:50:30 +0200

    remove our names from the code

--- src/yachtClub/Program.java
+++ src/yachtClub/Program.java
@@ -21,7 +21,7 @@ public class Program extends Application {
     	File file = new File("src/yachtClub.xml");
     	if (!file.exists()) ycDAO.jaxbObjectToXML(yachtClub);
     	yachtClub=ycDAO.jaxbXMLToObject();
-    	if (yachtClub.getMemberList().get(0).getId()!=1 || !yachtClub.getMember(1).getUserName().equals("alaa")) {yachtClub=new YachtClub();model.User.IDstatic=1;model.Boat.IDstatic=1;defaultProject(); ycDAO.jaxbObjectToXML(yachtClub);yachtClub=ycDAO.jaxbXMLToObject();}
+    	if (yachtClub.getMemberList().get(0).getId()!=1 || !yachtClub.getMember(1).getUserName().equals("aaaa")) {yachtClub=new YachtClub();model.User.IDstatic=1;model.Boat.IDstatic=1;defaultProject(); ycDAO.jaxbObjectToXML(yachtClub);yachtClub=ycDAO.jaxbXMLToObject();}
         launch(args);
     }
     
@@ -33,12 +33,12 @@ public class Program extends Application {
     
     private static void defaultProject() {
     	
-    	yachtClub.addMember(new Member("Alaa", "801201-5555", "alaa","alaa"));
-    	yachtClub.addMember(new Member("Amr","901102-6666", "amr","amr"));
+    	yachtClub.addMember(new Member("member1", "101010-5555", "aaaa","aaaa"));
+    	yachtClub.addMember(new Member("member2","111111-6666", "bbbb","bbbb"));
     	yachtClub.getMember(1).addBoat(new Boat(Type.Sailboat, 5,1));
     	yachtClub.getMember(1).addBoat(new Boat(Type.kayak, 4,1));
     	yachtClub.getMember(2).addBoat(new Boat(Type.Motorsailer, 3,2));
-    	yachtClub.setSecretary(new Secretary("Secretary", "850805-1111","secretary","secretary"));
+    	yachtClub.setSecretary(new Secretary("Secretary", "121212-1111","secretary","secretary"));
     }
 
 }
